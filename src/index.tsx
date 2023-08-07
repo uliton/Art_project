ae562952fd93633f484fba457485366ae77e51df
Oleksandr Kharutsa <al.haruca@gmail.com>
2023-08-07 12:39:12 +0200

    BrowserRouter change to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,13 @@
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import { App } from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
